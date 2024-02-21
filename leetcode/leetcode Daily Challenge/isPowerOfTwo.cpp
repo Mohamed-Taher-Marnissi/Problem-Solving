150fae67ea50c065de1a4b2a53d209ba1a37d662
Mohamed Taher Marnissi <137839316+Mohamed-Taher-Marnissi@users.noreply.github.com>
2024-02-21 21:06:44 +0100

    Update isPowerOfTwo.cpp

--- leetcode/leetcode Daily Challenge/isPowerOfTwo.cpp
+++ leetcode/leetcode Daily Challenge/isPowerOfTwo.cpp
@@ -1,1 +1,7 @@
-
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        // (n-1) and n equals 0, indicating that n has only one '1' bit in its binary representation (which is a characteristic of powers of two)
+        return (n > 0 && ((n-1)&n) == 0);
+    }
+};
