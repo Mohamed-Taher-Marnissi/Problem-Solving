b6e4ee3d45b8a927c735100e9396d7002e916a5b
Mohamed Taher Marnissi <137839316+Mohamed-Taher-Marnissi@users.noreply.github.com>
2024-03-01 18:34:29 +0100

    Update MaximumOddBinaryNumber.cpp

--- leetcode/leetcode Daily Challenge/MaximumOddBinaryNumber.cpp
+++ leetcode/leetcode Daily Challenge/MaximumOddBinaryNumber.cpp
@@ -1,1 +1,16 @@
-
+// problem link : https://leetcode.com/problems/maximum-odd-binary-number/
+class Solution {
+public:
+    string maximumOddBinaryNumber(string s) {
+        int res = count(s.begin() , s.end() , '1');
+        for(auto &c : s) {
+            if(c == '1') c = '0';
+        }
+        s[s.length() - 1] = '1';
+        res--;
+        for(int i = 0; i < res; i++) {
+            s[i] = '1';
+        }
+        return s;
+    }
+};
