29ccc73a5531bd0f52d8824bceabe2d08199248d
Mohamed Taher Marnissi <137839316+Mohamed-Taher-Marnissi@users.noreply.github.com>
2024-03-02 11:05:33 +0100

    Update sortedSquares.cpp

--- leetcode/leetcode Daily Challenge/sortedSquares.cpp
+++ leetcode/leetcode Daily Challenge/sortedSquares.cpp
@@ -1,1 +1,13 @@
 
+
+class Solution {
+public:
+
+    vector<int> sortedSquares(vector<int>& nums) {
+        for(int &i : nums) {
+            i = pow(i , 2);
+        }
+        sort(nums.begin() , nums.end());
+        return nums;
+    }
+};
