efb6f8c60a0edd3175e2f673a0460df4f40ba216
Mohamed Taher Marnissi <137839316+Mohamed-Taher-Marnissi@users.noreply.github.com>
2024-04-15 13:31:26 +0200

    Update 129.SumRootToLeftNumbers.cpp

--- leetcode/leetcode Daily Challenge/129.SumRootToLeftNumbers.cpp
+++ leetcode/leetcode Daily Challenge/129.SumRootToLeftNumbers.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int sum(TreeNode* root , int s=0) {
+        if(!root) return 0;
+        s = s*10 + root->val;
+        if(!root->left && !root->right) return s;
+        return sum(root->left , s) + sum(root->right , s);
+    }
+    int sumNumbers(TreeNode* root) {
+        return sum(root);
+    }
+};
